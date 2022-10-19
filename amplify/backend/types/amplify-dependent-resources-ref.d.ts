2d99fe106e5bfb0c180ad22e49f8845485a72441
Colin Barrett-Fox <colin@excitingtheory.com>
2022-10-19 01:10:12 -0700

    amplify is broken

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -6,8 +6,6 @@ export type AmplifyDependentResourcesAttributes = {
         "spiders17563e8247563e824": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
-            "HostedUIDomain": "string",
-            "OAuthMetadata": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
---
+++ pages/DOMAINS.md
@@ -0,0 +1,4 @@
+/<*>	https://dshcxqqs65rjx.cloudfront.net/<*>	200 (Rewrite)	-
+/domain/<*>	/domain/[id].html	200 (Rewrite)	-
+/domains/<*>	/domains/index.html	200 (Rewrite)	-
+/<*>	/index.html	404 (Rewrite)	-
