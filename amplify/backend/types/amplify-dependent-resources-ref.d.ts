fc23699fb4d1560bcc2b42edb7c665ed797f1b15
Colin Barrett-Fox <colin@excitingtheory.com>
2022-10-19 01:44:52 -0700

    rm a bunch of unused stuff

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,27 +1,18 @@
 export type AmplifyDependentResourcesAttributes = {
     "auth": {
-        "userPoolGroups": {
-            "freeGroupRole": "string"
-        },
         "spiders17563e8247563e824": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
             "AppClientIDWeb": "string",
-            "AppClientID": "string",
-            "HostedUIDomain": "string",
-            "OAuthMetadata": "string",
+            "AppClientID": "string"
         }
     },
     "function": {
-        "AdminQueriesd6d26a6b": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        },
         "spiderEngines": {
             "Name": "string",
             "Arn": "string",
@@ -42,11 +33,6 @@ export type AmplifyDependentResourcesAttributes = {
         }
     },
     "api": {
-        "AdminQueries": {
-            "RootUrl": "string",
-            "ApiName": "string",
-            "ApiId": "string"
-        },
         "spiders1": {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
